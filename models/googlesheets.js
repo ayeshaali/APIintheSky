5d3101520dc42d41a90eb00e38f9386927852b60
ayeshaali <aliayeshanyc@gmail.com>
2019-03-26 12:13:59 -0400

    message

--- models/googlesheets.js
+++ models/googlesheets.js
@@ -5,16 +5,6 @@ var dataJS = require(__dirname +'/data');
 var doc = new GoogleSpreadsheet('1DVgMG20OgfLR0leaJvzOiHDxp19EoyGKHTJxUCnxoX0');
 // Authenticate with the Google Spreadsheets API.
 
-exports.loadGoogle = function(filename, callback) {
-  var user_data = [];
-  doc.useServiceAccountAuth(creds, function (err) {
-    doc.getRows(filename, function (err, rows) {
-      callback(rows);
-    });
-  });
-}
-
-
 exports.getDataJob = function(p,p1,p2,p3,p4,p5){
   var a = [p1,p2,p3,p4,p5];
 
@@ -25,12 +15,14 @@ exports.getDataProgram = function(p,p1,p2,p3,p4,p5){
   var a = [p1,p2,p3,p4,p5]
 }
 
-
-
-
-
-
-
+exports.loadGoogle = function(filename, callback) {
+  var user_data = [];
+  doc.useServiceAccountAuth(creds, function (err) {
+    doc.getRows(filename, function (err, rows) {
+      callback(rows);
+    });
+  });
+}
 
 //Updates a row
 exports.updateRow=function(filename, userName, newStuff, callback){
@@ -95,76 +87,4 @@ exports.deleteRow = function(user_id, callback) {
       });
     });
   });
-}
-
-exports.log=function(message){
-    console.log(message);
-    doc.useServiceAccountAuth(creds, function (err) {
-        doc.getInfo(function(err,info){
-    sheet=info.worksheets[2];
-      sheet.getCells({
-        'min-col': 1,
-        'max-col': 1,
-        'return-empty': true}, function(err, cells) {
-          go=true;
-          i=0;
-        while (go){
-            var a="";
-            try{a=cells[i].value}catch(ErrorEvent){}
-            if(a==""){
-          cells[i].setValue(JSON.stringify(message));
-                go=false;
-            }else{
-            i++;
-            }
-        }
-      });
-      });
-  });
-}
-
-exports.increment=function(page){
-    arr=["index","game","results","rules","stats","about","userDetails"];
-    num=arr.indexOf(page)+4;
-    doc.useServiceAccountAuth(creds, function (err) {
-        doc.getInfo(function(err,info){
-        sheet=info.worksheets[2];
-      sheet.getCells({
-        'min-col': num,
-        'max-col': num,
-        'return-empty': true}, function(err, cells) {
-            var a=0;
-            try{a=parseInt(cells[1].value)}catch(ErrorEvent){}
-            if(a==0){
-          cells[1].setValue(1);
-            }else{
-            cells[1].setValue(a+1);
-            }
-      });
-      });
-  });
-}
-
-exports.loadUsage=function(callback){
-    arr=[0,0,0,0,0,0,0];
-    doc.useServiceAccountAuth(creds, function (err) {
-        doc.getInfo(function(err,info){
-        sheet=info.worksheets[2];
-
-        sheet.getCells({
-        'min-row':2,
-        'min-col': 4,
-        'max-col': 10,
-        'max-row':2,
-        'return-empty': true}, function(err, cells) {
-            for(var i=0; i<7;i++){
-            arr[i]=parseInt(cells[i].value);
-            }
-            callback(arr);
-      });
-
-      });
-
-  });
-
 }
