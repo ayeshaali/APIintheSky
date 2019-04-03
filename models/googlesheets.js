47a01ed53c97dcf6cc69b29bddfb49bdf7c9db2d
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2019-04-03 12:06:56 -0400

    nodeandsheet

--- models/googlesheets.js
+++ models/googlesheets.js
@@ -5,16 +5,6 @@ var creds = require('../client_secret.json');
 var doc = new GoogleSpreadsheet('1DVgMG20OgfLR0leaJvzOiHDxp19EoyGKHTJxUCnxoX0');
 // Authenticate with the Google Spreadsheets API.
 
-exports.getDataJob = function(p,p1,p2,p3,p4,p5){
-  var a = [p1,p2,p3,p4,p5];
-
-  //long function
-}
-
-exports.getDataProgram = function(p,p1,p2,p3,p4,p5){
-  var a = [p1,p2,p3,p4,p5]
-}
-
 exports.loadGoogle = function(filename, callback) {
   var user_data = [];
   doc.useServiceAccountAuth(creds, function (err) {
@@ -24,23 +14,6 @@ exports.loadGoogle = function(filename, callback) {
   });
 }
 
-exports.getAllKeys = function(callback) {
-  doc.useServiceAccountAuth(creds, function (err) {
-    doc.getInfo(function(err,info){
-      sheet=info.worksheets[1];
-      sheet.getCells({
-        'min-col': 3,
-        'max-col': 3,
-        'return-empty': true}, function(err, cells){
-          var cell = [];
-          for (var i=0; i<cells.length; i++) {
-            cell[i]=cells[i].value;
-          }
-          callback(cell);
-        }
-    });
-  });
-}
 //Updates a row
 exports.updateRow=function(filename, userName, newStuff, callback){
   var sheet;
@@ -70,10 +43,10 @@ exports.updateRow=function(filename, userName, newStuff, callback){
 }
 
 // creates a new row (when making a new user)
-exports.createRow = function(obj, callback) {
+exports.createRow = function(obj, filenumber, callback) {
   var sheet;
   doc.useServiceAccountAuth(creds, function (err) {
-    doc.addRow(1, obj, function(){
+    doc.addRow(filenumber, obj, function(){
       dataJS.log("Calling first callback")
       callback();
     });
--- models/jobprogram.js
+++ models/jobprogram.js
@@ -4,6 +4,32 @@ var creds = require('../client_secret.json');
 var fs = require("fs");
 var dataJS = require(__dirname +'/googlesheets');
 var User = require(__dirname +'/User');
+var Papa = require("papaparse");
+
+
+//need to test Papaparse. This takes in the csv file, the sheet (Filename) and callback then we use Papaparse to parse through CSV rows step-by-step for large data set and then add them to the sheet
+exports.loadSheetToPage = function(csv, filename, callback){
+  dataJS.loadGoogle(filename, function(){
+    Papa.parse((this.csv).files[0]),{
+      step:function(row){
+        dataJS.createRow(filename, row, function(){
+          console.log("Row has been added to csv");
+        });
+      },
+      complete:function(){
+        console.log("This parse has been completed and loadSheet")
+      }
+    }
+  });
+}
+
+
+
+
+
+
+
+
 
 
 
