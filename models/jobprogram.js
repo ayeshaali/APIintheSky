5e9e8ad1d423cf86e46eda4e48016af8653b6d3c
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2019-04-05 16:47:24 -0400

    modelsedit

--- models/jobprogram.js
+++ models/jobprogram.js
@@ -5,8 +5,11 @@ var fs = require("fs");
 var dataJS = require(__dirname +'/googlesheets');
 var Users = require(__dirname +'/User');
 var Papa = require("papaparse");
+var doc = new GoogleSpreadsheet('1DVgMG20OgfLR0leaJvzOiHDxp19EoyGKHTJxUCnxoX0');
 
-//This takes in a CSV(Jobs/Programs), a Sheet to Edit, A
+//This takes in a CSV(Jobs/Programs), a Sheet to Edit, An APIKEY and a callback function.
+//First you check the API key to load the page. If the k contains. We clear the sheet off screen and then parse the CSV and create a sheet row by row.
+
 exports.loadSheetToPage = function(csv, filename, apikey, callback){
   var k = checkAPI(apikey);
 if(k==true){
@@ -26,12 +29,48 @@ if(k==true){
 });
   callback();
 }
+else{
+  //return error;
 }
-//just send param through array
+}
+//This takes in a CSV, filename, an array of parameters, an apikey and a callback.
+//It checks to see if APIkey works, then we clear sheet, parse the CSV and run a for loop to check if each parameter in the row "matches" the param array. We will ahve to figure that out to compare data but if it's true for how many ever elemtns in array. The info will be added by a row.
+
+
 exports.selectParameter= function(csv, filename, param, apikey, callback){
+  //var meetsConditions = false;
   var k = checkAPI(apikey);
   if(k==true){
-
+      dataJS.clearSheet(filename,function(){
+        Papa.parse((this.csv).files[0], {
+          step: function(row){
+            for(var i=0; i<param.length; i++){
+              doc.getCells(1, {'min-col': param[0], 'max-col': param[param.length]}, function(err, cells){
+                  if(cells[i] == param[i]){
+                    if(i==param.length){
+                        dataJS.createRow(this.filename, this.row, function(){
+                          console.log("Row has been added to CSV with Param selector");
+                        });
+                    }
+                    else{
+                      continue;
+                    }
+                  }
+                  else{
+                    break;
+                  }
+              });
+            }
+          },
+          complete: function(){
+            console.log("This parse and clear has been complete");
+          }
+        });
+      });
+    callback();
+  }
+  else{
+    //return error;
   }
 }
 
@@ -46,21 +85,3 @@ function checkAPI(apikey){
   //put something in here to check the API key
 }
 console.log("Truth for API Check" + truth);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/
