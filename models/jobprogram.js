90a7d48a7be16f7c9197cacb7ff35b08dc7be710
Mit Ramesh <mithun.ramesh19@trinityschoolnyc.org>
2019-04-09 17:51:22 -0400

    jsonidea

--- models/jobprogram.js
+++ models/jobprogram.js
@@ -6,15 +6,47 @@ var dataJS = require(__dirname +'/googlesheets');
 var Users = require(__dirname +'/User');
 var Papa = require("papaparse");
 var doc = new GoogleSpreadsheet('1DVgMG20OgfLR0leaJvzOiHDxp19EoyGKHTJxUCnxoX0');
+var GSParser = require ('google-spreadsheets-parser');
+
+
+
+//make Param an object array
+exports.ParamSelec = function(filename, param, apikey, callback){
+  var jsonObj = {};
+  var k = checkAPI(apikey);
+    if(k==true){
+      dataJS.loadGoogle(filename,function(){
+        doc.getRows(filename, function(err,rows){
+          for(var i =0; i<rows.length; i++){
+            jsonObj.push(rows[i]);
+          }
+        });
+
+        //check the JSON for paramters. If true then change the sheet to reflect data and then send JSON
+      });
+  return jsonObj;
+}
+
+function checkAPI(apikey){
+  var k = Users.getUser(apikey);
+  if(k==true){
+      return true;
+  }
+  else{
+    return false;
+  }
+  //put something in here to check the API key
+}
+console.log("Truth for API Check" + truth);
 
 
 
 
 
-
+//---------DELETED CODE-----------------------------------------------------------------------
 //This takes in a CSV(Jobs/Programs), a Sheet to Edit, An APIKEY and a callback function.
 //First you check the API key to load the page. If the k contains. We clear the sheet off screen and then parse the CSV and create a sheet row by row.
-
+/*
 exports.loadSheetToPage = function(csv, filename, apikey, callback){
 var k = checkAPI(apikey);
 if(k==true){
@@ -40,31 +72,34 @@ else{
   //return error;
 }
 }
-
-
-
-
-
-
-
-
 //This takes in a CSV, filename, an array of parameters, an apikey and a callback.
 //It checks to see if APIkey works, then we clear sheet, parse the CSV and run a for loop to check if each parameter in the row "matches" the param array. We will ahve to figure that out to compare data but if it's true for how many ever elemtns in array. The info will be added by a row.
-exports.ParamSelec = function(filename, param, apikey, callback){
+exports.ParamSelec = function(filename, headerlist, param, apikey, callback){
 var JSONObject  = {};
   var k = checkAPI(apikey);
     if(k==true){
       dataJS.loadGoogle(filename,function(){
-          //doc.getCells(1, {'min-col': 0, 'max-col': param.length}, function(err, cells){
+        doc.getRows(filename, {query:} function(err,rows){
+
+
       });
-    }
+
+
+          for(var i=0; i<rows.length; i++){
+            doc.getCells(filename,{'min-col': param[0], 'max-col': param[param.length]}, function(err, cells){
+
+            });
+          }
+        });
+      });
+      */
+    });
 }
 
 
 
 
-
-
+/*
 exports.selectParameter= function(csv, filename, param, apikey, callback){
   //var meetsConditions = false;
   var k = checkAPI(apikey);
@@ -81,7 +116,7 @@ exports.selectParameter= function(csv, filename, param, apikey, callback){
                         dataJS.createRow(this.filename, this.row, function(){
                           console.log("Row has been added to CSV with Param selector");
                         });
-                        */
+
                     }
                     else{
 
@@ -105,17 +140,4 @@ exports.selectParameter= function(csv, filename, param, apikey, callback){
     //return error;
   }
 }
-
-
-
-function checkAPI(apikey){
-  var k = Users.getUser(apikey);
-  if(k==true){
-      return true;
-  }
-  else{
-    return false;
-  }
-  //put something in here to check the API key
-}
-console.log("Truth for API Check" + truth);
+*/
