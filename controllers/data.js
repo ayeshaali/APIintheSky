475af2ca52a2923b5d2ea95209c6032510fbe011
Ayesha <aliayeshanyc@gmail.com>
2019-04-10 08:21:58 -0400

    data routes

--- controllers/data.js
+++ controllers/data.js
@@ -1,1 +1,32 @@
-//routes to be requested by the web app
+var express = require('express');
+var fs = require("fs");
+var router = express.Router();
+var dataJS = require('../models/googlesheets');
+var jobJS = require('../models/jobprogram');
+
+router.get('/benefitsearch', function(request, response) {
+  var data={
+    apikey: request.query.apikey,
+    name: request.query.name,
+    type: request.query.type,
+    pop: request.query.pop,
+    contact: request.query.contact,
+    desc: request.query.desc,
+  };
+  
+  jobJS.
+  
+})
+
+router.get('/jobsearch', function(request, response) {
+  var data={
+    apikey: request.query.apikey,
+    agency: request.query.agency,
+    title: request.query.title,
+    category: request.query.category,
+    service: request.query.service,
+    location: request.query.location,
+  };
+  
+  
+})
