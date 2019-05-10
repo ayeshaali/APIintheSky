33f9ae9f87a68db6d919bd36165853a8729d16bf
ayeshaali <aliayeshanyc@gmail.com>
2019-05-10 18:51:42 -0400

    data for jobs works!

--- models/jobprogram.js
+++ models/jobprogram.js
@@ -43,12 +43,9 @@ exports.ParamSelec = function(filename, param, apikey, callback){
       dataJS.loadGoogle(filename,function(rows){
           for(var i =0; i<rows.length; i++){
             var value = Object.values(rows[i]);
-            for(var k=0; k<value.length; k++){
-              console.log("Values object for this row" + value[k]);
-            }
             if(filename==1){
-              jsonObj = {"agency": value[4], "business_title": value[5], "job_category": value[7], "part_or_full": value[8], "Location": value[12], "ID": value[13]};
-              console.log(jsonObj);
+              jsonObj = {"agency": value[4], "business_title": value[5], "job_category": value[7], "part_or_full": value[8], "location": value[12], "ID": value[13]};
+              // console.log(jsonObj);
             } else{
               jsonObj = {"program_name": value[1], "benefit_type": value[2]  , "population_served": value[5] , "contact_info":value[9] ,  "summary": value[7], "ID": value[8]};
             }
